33e106433e36b7ee0afdf8b203da539e5dfd6c0f
Rodrigo Matsumoto <rodrigo.matsumoto87@gmail.com>
2022-04-26 01:30:47 -0300

    criado models e personalidades.go, atualizado controllers e routes para responder json

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/RodrigoMatsumoto/go-rest-api/models"
 	"github.com/RodrigoMatsumoto/go-rest-api/routes"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
